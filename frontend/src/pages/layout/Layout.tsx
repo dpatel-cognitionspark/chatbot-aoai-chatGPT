8400f4cbfd543d0144019115bda2696b4039ccd8
Dharmesh <dharmesh019@gmail.com>
2024-01-11 19:21:38 +1100

    put back layout logic

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -1,12 +1,51 @@
 import { Outlet, Link } from "react-router-dom";
 import styles from "./Layout.module.css";
+import Contoso from "../../assets/Contoso.svg";
 import { CopyRegular, ShareRegular } from "@fluentui/react-icons";
+import { Dialog, Stack, TextField, ICommandBarStyles, IButtonStyles } from "@fluentui/react";
+import { useContext, useEffect, useState } from "react";
+import { HistoryButton, ShareButton } from "../../components/common/Button";
+import { AppStateContext } from "../../state/AppProvider";
+import { CosmosDBStatus } from "../../api";
 
 const Layout = () => {
-   
+    const [copyClicked, setCopyClicked] = useState<boolean>(false);
+    const [copyText, setCopyText] = useState<string>("Copy URL");
+    const appStateContext = useContext(AppStateContext)
+
+    
+
+    const handleSharePanelDismiss = () => {
+        setCopyClicked(false);
+        setCopyText("Copy URL");
+    };
+
+    const handleCopyClick = () => {
+        navigator.clipboard.writeText(window.location.href);
+        setCopyClicked(true);
+    };
+
+    const handleHistoryClick = () => {
+        appStateContext?.dispatch({ type: 'TOGGLE_CHAT_HISTORY' })
+    };
+
+    useEffect(() => {
+        if (copyClicked) {
+            setCopyText("Copied URL");
+        }
+    }, [copyClicked]);
+
+    useEffect(() => { }, [appStateContext?.state.isCosmosDBAvailable.status]);
+
     return (
         <div className={styles.layout}>
-         ' '
+            <header className={styles.header} role={"banner"}>
+                <Stack horizontal verticalAlign="center" horizontalAlign="space-between">
+                   
+
+                </Stack>
+            </header>
+            <Outlet />
         </div>
     );
 };
